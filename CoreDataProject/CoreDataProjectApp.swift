6bc45ab9f02db1fe215176237fd7194a0e8c2add
Alison Gorman <alison@minimascientific.com>
2022-11-02 13:10:55 -0600

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Alison Gorman on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
