700cbcfbb6c089d7b6dadb3495b256ceff56ab61
Alison Gorman <alison@minimascientific.com>
2022-11-03 10:20:15 -0600

    Day 57 - Project 12, part 1 - NSManagedObject subclasses, conditional saving, ensuring unique core data objects using constraints

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -5,22 +5,33 @@
 //  Created by Alison Gorman on 11/2/22.
 //
 
+
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+
+    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "Unknown")
+            }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Button("Add") {
+                let wizard = Wizard(context: moc)
+                wizard.name = "Harry Potter"
+            }
+
+            Button("Save") {
+                do {
+                    try moc.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
